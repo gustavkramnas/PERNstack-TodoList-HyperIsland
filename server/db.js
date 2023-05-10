df41b43ce14ca00e3483eeb5c65c0874bda39b50
Gustav <gustav@kramnas.se>
2023-05-10 17:27:57 +0200

    adding to env file

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   password: process.env.PASSWORD,
   host: process.env.HOST,
   port: process.env.PORT,
-  database: "pernstacktodo",
+  database: process.env.DATABASE,
 });
 
 module.exports = pool;
